2f4d3e3a476d3f6127e3749648802045eb05b458
jyn <spdlqjdkslel1226@naver.com>
2025-11-12 20:01:09 +0900

    feat: 유저 등록 컨트롤러 작성
    - 데이터는 서버 내 파일 activatedRooms.json에서 관리함.
    - addRoom: 남은 방, 활성화 방 목록 업데이트
    - registerUser: rooms 목록에 유저 데이터 추가
    - registerGuest: 만석일 경우, guests 목록에 게스트 추가 (게스트로 참여)

---
+++ src/utils/getPathName.js
@@ -0,0 +1,12 @@
+import path from "path";
+import { fileURLToPath } from "url";
+// import.meta.url 기준으로 현재 파일(server.js) 디렉토리 가져오기
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const getPath = (dataPath) => {
+  // JSON 파일 경로
+  const DATA_FILE = path.join(__dirname, dataPath);
+  return DATA_FILE;
+};
