5b09a73649dcf098aa6594cf01b256f156546537
jyn <spdlqjdkslel1226@naver.com>
2025-11-16 21:23:18 +0900

    modify: 환경변수 사용

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,8 +1,5 @@
 export const corsOptions = {
-  origin: [
-    "https://wtcom-still-awake-frontend.vercel.app",
-    "http://localhost:3000",
-  ],
+  origin: [process.env.CLIENT_ORIGIN, "http://localhost:3000"],
   methods: ["GET", "POST"],
   credentials: true,
 };
