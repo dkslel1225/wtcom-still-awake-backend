1a91bed25b5cbeea26a5ab0756cb12637b1e6c8c
jyn <spdlqjdkslel1226@naver.com>
2025-11-18 03:32:18 +0900

    fix: OPTIONS 메소드 허용

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
   origin: [process.env.CLIENT_ORIGIN, "http://localhost:3000"],
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "OPTIONS"],
   credentials: true,
 };
